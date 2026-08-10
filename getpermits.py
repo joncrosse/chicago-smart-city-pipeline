3f16851842eca9e9786285ce719059d1a8dc9ed6
Jon Crosse <jon.crosse@aol.com>
2026-08-10 18:01:56 -0500

    First commit: add existing ingestion scripts and gitignore

---
+++ getpermits.py
@@ -0,0 +1,62 @@
+import os
+import requests
+import json
+from datetime import datetime
+from google.cloud import storage
+
+API_URL = "https://data.cityofchicago.org/resource/pubx-yq2d.json"
+PROJECT_ID = "project-2074846a-2a76-442f-b1f"
+BUCKET_NAME = "cdot_data_lake_8751"
+
+def fetch_chicago_permits():
+    
+
+    params = {
+        "$limit": 7500,
+        "$order": "APPLICATIONSTARTDATE DESC"
+    }
+
+    try:
+        print(f"[{datetime.now()}] Fetching active permit data from CDOT...")
+        response = requests.get(API_URL, params=params)
+        response.raise_for_status()
+
+        raw_data = response.json()
+        print(f"Successfully retrieved {len(raw_data)} records.")
+
+        #os.makedirs("data/raw/chicago_permits", exist_ok=True)
+        #timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        #filename = f"data/raw/chicago_permits/permits_{timestamp}.json"
+
+        #with open(filename, "w") as f:
+        #    json.dump(raw_data, f, indent=4)
+
+        #print(f"Data landed locally at: {filename}")
+
+        return raw_data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Pipeline Execution Failed: {e}")
+        return None
+
+def upload_permits_to_gcs(raw_data):
+    storage_client = storage.Client(project=PROJECT_ID)
+    bucket = storage_client.bucket(BUCKET_NAME)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    time_path = datetime.now().strftime("%Y/%m/%d")
+    blob_name = f"raw/chicago_permits/{time_path}/permits_{timestamp}.json"
+
+    blob = bucket.blob(blob_name)
+    
+    blob.upload_from_string(
+        data=json.dumps(raw_data, indent=4),
+        content_type='application/json'
+    )
+
+if __name__ == "__main__":
+    chicago_permits = fetch_chicago_permits()
+    if chicago_permits:
+        upload_permits_to_gcs(chicago_permits)
+    else:
+        print("No Chicago permit data was retrieved, upload skipped.")
---
+++ ingest_cta_trains.py
@@ -0,0 +1,65 @@
+
+import os
+import requests
+import json
+from datetime import datetime
+from google.cloud import storage
+
+API_KEY = "config"
+PROJECT_ID = "config"
+BUCKET_NAME = "config"
+
+def fetch_cta_trains():
+
+    api_url = "http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?"
+
+    params = {
+        "key": API_KEY,
+        "rt": "red,blue,brn,g,org,p,pink,y",
+        "outputType": 'JSON'
+    }
+
+    try:
+        print("Getting data from CTA...")
+
+        response = requests.get(api_url, params=params)
+        response.raise_for_status()
+
+        raw_data = response.json()
+
+        print(f"Successfully retrieved {len(raw_data["ctatt"]["route"])} train routes.")
+
+        os.makedirs("data/raw/cta_trains/test_trains", exist_ok=True)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"data/raw/cta_trains/test_trains/trains_{timestamp}.json"
+
+        with open(filename,"w") as f:
+            json.dump(raw_data, f, indent=4)
+
+        return raw_data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Pipeline Execution Failed: {e}")
+        return None
+
+def upload_cta_to_gcs(raw_data):
+    storage_client = storage.Client(project=PROJECT_ID)
+    bucket = storage_client.bucket(BUCKET_NAME)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    time_path = datetime.now().strftime("%Y/%m/%d")
+    blob_name = f"raw/cta_trains/{time_path}/trains_{timestamp}.json"
+    blob = bucket.blob(blob_name)
+
+    blob.upload_from_string(
+        data=json.dumps(raw_data, indent=4),
+        content_type='application/json'
+    )
+
+
+if __name__ == "__main__":
+    cta_data = fetch_cta_trains()
+    if cta_data:
+        upload_cta_to_gcs(cta_data)
+    else:
+        print("No CTA data was retrieved, upload skipped.")
